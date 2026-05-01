56c3b4d7ab55eee2fcf3f9584134233c466dad9a
[dyad] <jkrmao@gmail.com>
2026-05-01 02:44:23 +0200

    [dyad] Aggiornato codice per usare variabili Vercel - wrote 4 file(s)

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
-/**
- * Webpack configuration for Angular Component Tagging
- * This enables Select UI to Edit feature in Dyad.sh
- */
-
 const AngularComponentTagger = require('./angular-webpack-component-tagger');
+const webpack = require('webpack');
 
 module.exports = {
   plugins: [
-    new AngularComponentTagger()
+    new AngularComponentTagger(),
+    new webpack.DefinePlugin({
+      'process.env.NEXT_PUBLIC_SUPABASE_URL': JSON.stringify(
+        process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://xccigtseyhdmpwdlsijv.supabase.co'
+      ),
+      'process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY': JSON.stringify(
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhjY2lndHNleWhkbXB3ZGxzaWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxMjU3MzIsImV4cCI6MjA5MjcwMTczMn0.gwV43Z_AsuhpNemzs4FfWI97h4ZdQZ0vjRbN26pqVzg'
+      )
+    })
   ]
 };
